377cb944fa28b03996bbdb6370531dacef3ab615
gustavo.caruso <gustavoalmeida26@hotmail.com>
2021-11-16 10:21:17 -0300

    update

--- src/components/AgendamentoConsulta.jsx
+++ src/components/AgendamentoConsulta.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import { Context } from '../context/Context';
 import { agenda, specialities } from '../services/data';
 
